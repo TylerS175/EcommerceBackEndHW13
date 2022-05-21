8250845ee15c279622a9f89594dd5c815c316ba3
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-05-21 19:21:37 -0400

    Create Category init

--- models/Category.js
+++ models/Category.js
@@ -1,3 +1,4 @@
+const { type } = require('express/lib/response');
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
@@ -6,7 +7,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataType.INTEGER,
+    allowNull: false, 
+    primaryKey: true, 
+    autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+
   },
   {
     sequelize,
